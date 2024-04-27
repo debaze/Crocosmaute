5ae85043f7be6e998dcc4d9d79f639e045a488f8
matteokeole <matteo@keole.net>
2024-04-27 19:05:26 +0200

    style: Prepare for implementation

--- Patches/BoomboxPatch.cs
+++ Patches/BoomboxPatch.cs
@@ -5,9 +5,9 @@ namespace Crocosmaute.Patches;
 
 [HarmonyPatch(typeof(BoomboxItem))]
 public class BoomboxPatch {
-    [HarmonyPatch("Start")]
-    [HarmonyPostfix]
-    private static void StartPostfix(ref AudioSource ___boomboxAudio) {
-        ___boomboxAudio.volume = 0.2f;
-    }
+	[HarmonyPatch("Start")]
+	[HarmonyPostfix]
+	private static void StartPostfix(ref AudioSource ___boomboxAudio) {
+		//
+	}
 }
