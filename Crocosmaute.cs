2edd4056b269b049701411b183081b26926636c3
matteokeole <matteo@keole.net>
2024-05-01 13:50:36 +0200

    refactor: Variable naming

--- Crocosmaute.cs
+++ Crocosmaute.cs
@@ -10,34 +10,44 @@ namespace Crocosmaute;
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 [BepInDependency("BMX.LobbyCompatibility", BepInDependency.DependencyFlags.HardDependency)]
 [LobbyCompatibility(CompatibilityLevel.ClientOnly, VersionStrictness.None)]
-public class Crocosmaute : BaseUnityPlugin
-{
-	public static Crocosmaute Instance { get; private set; } = null!;
-	internal new static ManualLogSource Logger { get; private set; } = null!;
-	internal static Harmony? Harmony { get; set; }
+public class Crocosmaute : BaseUnityPlugin {
+	public static Crocosmaute Instance {
+		get; private set;
+	} = null!;
+
+	internal new static ManualLogSource Logger {
+		get;
+		private set;
+	} = null!;
+
+	internal static Harmony? Harmony {
+		get;
+		set;
+	}
+
 	internal static AudioClip[] audioClips;
 
-	private void Awake()
-	{
+	private void Awake() {
 		Logger = base.Logger;
 		Instance = this;
-		Logger.LogInfo(((BaseUnityPlugin)Instance).Info.Location);
-		AssetBundle val = AssetBundle.LoadFromFile(((BaseUnityPlugin)Instance).Info.Location.TrimEnd(".dll".ToCharArray()));
-		if (val == null)
-		{
-			Logger.LogError((object)"Failed to load audio assets!");
+
+		string assetBundlePath = Instance.Info.Location.TrimEnd(".dll".ToCharArray());
+		AssetBundle assetBundle = AssetBundle.LoadFromFile(assetBundlePath);
+
+		if (assetBundle == null) {
+			Logger.LogError("Failed to load AssetBundle.");
+
 			return;
 		}
 
-		audioClips = val.LoadAllAssets<AudioClip>();
+		audioClips = assetBundle.LoadAllAssets<AudioClip>();
 
 		Patch();
 
 		Logger.LogInfo($"{MyPluginInfo.PLUGIN_GUID} v{MyPluginInfo.PLUGIN_VERSION} has loaded!");
 	}
 
-	internal static void Patch()
-	{
+	internal static void Patch() {
 		Harmony ??= new Harmony(MyPluginInfo.PLUGIN_GUID);
 
 		Logger.LogDebug("Patching...");
@@ -47,8 +57,7 @@ public class Crocosmaute : BaseUnityPlugin
 		Logger.LogDebug("Finished patching!");
 	}
 
-	internal static void Unpatch()
-	{
+	internal static void Unpatch() {
 		Logger.LogDebug("Unpatching...");
 
 		Harmony?.UnpatchSelf();
